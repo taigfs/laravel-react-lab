3580c151391b4c7d31d11a0554e94e985a86027d
Tainan Fidelis <taigfs@gmail.com>
2022-04-21 13:57:03 -0300

    changing the identifier parameter handler; using compiled react app files

--- blog/resources/views/lesson-list.blade.php
+++ blog/resources/views/lesson-list.blade.php
@@ -5,10 +5,12 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Laravel</title>
-        <link href="{{asset('css/app.css')}}" rel="stylesheet" type="text/css">
+        <link href="{{asset('static/css/main.min.css')}}" rel="stylesheet" type="text/css">
     </head>
     <body>
-        <div id="lesson-list-page" data="{{ $data }}"></div>
-        <script src="{{asset('js/app.js')}}" ></script>
+        <!-- <div id="lesson-list-page" data="{{ $data }}"></div> -->
+        <!-- <script src="{{asset('js/app.js')}}" ></script> -->
+        <div id="root" data="{{ $data }}"></div>
+        <script src="{{asset('static/js/main.min.js')}}" ></script>
     </body>
 </html>
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -12,9 +12,9 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome')->with('data', json_encode(['page_id' => 'root']));
 });
 
 Route::get('/lesson-list', function () {
-    return view('lesson-list')->with('data', json_encode(['lessons' => ['Lesson A', 'Lesson B', 'Lesson C']]));
+    return view('lesson-list')->with('data', json_encode(['page_id' => 'lesson-list', 'lessons' => ['Lesson A', 'Lesson B', 'Lesson C']]));
 });
